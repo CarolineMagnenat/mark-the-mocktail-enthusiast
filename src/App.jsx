01a83ecf7ea4f8970b31afc54a11e40ae83e615a
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-09-17 14:10:48 +0200

    hålla hallå console.log

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
   );
   const [activeFilter, setActiveFilter] = useState(() => (drink) => drink); //returns a 1 to 1 function
 
+
+  console.log('hallå hallå')
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
