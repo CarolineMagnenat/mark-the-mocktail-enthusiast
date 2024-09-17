6586c4885441661d5394cce5171680e97dd4f60e
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-09-17 15:06:26 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
   );
   const [activeFilter, setActiveFilter] = useState(() => (drink) => drink); //returns a 1 to 1 function
 
+  console.log('nummer två')
+
   console.log('master master')
 
   console.log('hallå hallå')
